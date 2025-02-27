64aa3d10dfa1f27556078c905c3cd20df92151bb
Sebastian Górski <sebastiangorski00@gmail.com>
2025-02-27 11:16:24 +0100

    roles in applicaiton user

--- src/main/java/pl/studia/InstaCar/model/authentication/ApplicationUser.java
+++ src/main/java/pl/studia/InstaCar/model/authentication/ApplicationUser.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -35,7 +37,7 @@ public class ApplicationUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getName().toUpperCase()));
     }
 
     @Override
