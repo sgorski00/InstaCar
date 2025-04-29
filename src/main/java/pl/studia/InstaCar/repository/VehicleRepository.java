94c42215814c07b389f4ee020b06ea70f4ccd435
Sebastian Górski <sebastiangorski00@gmail.com>
2025-04-29 12:40:35 +0200

    cars search

--- src/main/java/pl/studia/InstaCar/repository/VehicleRepository.java
+++ src/main/java/pl/studia/InstaCar/repository/VehicleRepository.java
@@ -1,7 +1,5 @@
 package pl.studia.InstaCar.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,4 +17,13 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
         LOWER(CONCAT(v.model.brand,' ',v.model.modelName)) LIKE %:query%
         """)
     List<Vehicle> findAllByQuery(@Param("query") String query);
+
+    @Query("""
+        SELECT v FROM Vehicle v
+        WHERE (LOWER(v.model.brand) LIKE %:query% OR
+        LOWER(v.model.modelName) LIKE %:query% OR
+        LOWER(CONCAT(v.model.brand,' ',v.model.modelName)) LIKE %:query%)
+        AND (:type = 'all' OR LOWER(v.class) LIKE %:type%)
+        """)
+    List<Vehicle> findAllByQueryAndType(@Param("query") String query, @Param("type") String type);
 }
