e0f61958ad24604e19dfedb3eb9aa4014d7933de
Sebastian Górski <sebastiangorski00@gmail.com>
2025-05-06 10:59:34 +0200

    connected car

--- src/main/java/pl/studia/InstaCar/model/dto/RateResponse.java
+++ src/main/java/pl/studia/InstaCar/model/dto/RateResponse.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
 
 @Data
@@ -15,4 +18,11 @@ public class RateResponse {
     private String currency;
     private String code;
     private List<Rate> rates;
+
+    public double getNewestPrice(double price) {
+        NumberFormat nf = new DecimalFormat("#0.00");
+        nf.setRoundingMode(RoundingMode.HALF_EVEN);
+        double result = Double.parseDouble(rates.getLast().getMid()) * price;
+        return Double.parseDouble(nf.format(result));
+    }
 }
