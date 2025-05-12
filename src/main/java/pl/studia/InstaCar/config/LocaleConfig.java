ba52fdc77f84a311e4b80d272805bf08e65a6f2a
Sebastian Górski <sebastiangorski00@gmail.com>
2025-05-12 12:53:28 +0200

    translation of all bodyfragments

--- src/main/java/pl/studia/InstaCar/config/LocaleConfig.java
+++ src/main/java/pl/studia/InstaCar/config/LocaleConfig.java
@@ -1,7 +1,10 @@
 package pl.studia.InstaCar.config;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,17 +14,20 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @Configuration
+@Log4j2
 public class LocaleConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+        log.info("Interceptor added");
     }
 
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.of("pl"));
+        slr.setDefaultLocale(Locale.forLanguageTag("pl"));
+        log.info("LocaleResolver added");
         return slr;
     }
 
@@ -29,6 +35,15 @@ public class LocaleConfig implements WebMvcConfigurer {
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
+        log.info("LocaleChangeInterceptor added");
         return lci;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("classpath:messages");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
     }
 }
